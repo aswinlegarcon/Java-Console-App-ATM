3a15667f28a2a59dedeb1ccf9539ccd994231eda
Aswin Kirubanantham <aswinkirubanantham@gmail.com>
2025-01-04 23:50:10 +0530

    Delete ATM/Notes/FiveHundred.java

--- ATM/Notes/FiveHundred.java
+++
@@ -1,9 +0,0 @@
-package ATM.Notes;
-
-public class FiveHundred extends Notes{
-    public FiveHundred(String note,int count)
-    {
-        this.setNote(note);
-        this.setCount(count);
-    }
-}
